dba95db46394f668d400a9253b21f555bbce0556
Robin Genz <mail@robingenz.dev>
2022-03-30 15:50:01 +0200

    feat(authentication): provide access token for Google (Android)

--- packages/authentication/android/src/main/java/dev/robingenz/capacitorjs/plugins/firebase/authentication/handlers/GoogleAuthProviderHandler.java
+++ packages/authentication/android/src/main/java/dev/robingenz/capacitorjs/plugins/firebase/authentication/handlers/GoogleAuthProviderHandler.java
@@ -1,20 +1,23 @@
 package dev.robingenz.capacitorjs.plugins.firebase.authentication.handlers;
 
 import android.content.Intent;
+import android.os.Bundle;
 import androidx.activity.result.ActivityResult;
-import com.getcapacitor.JSObject;
 import com.getcapacitor.PluginCall;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.OAuthCredential;
 import dev.robingenz.capacitorjs.plugins.firebase.authentication.FirebaseAuthentication;
 import dev.robingenz.capacitorjs.plugins.firebase.authentication.R;
+import java.io.IOException;
 
 public class GoogleAuthProviderHandler {
 
@@ -46,9 +49,23 @@ public class GoogleAuthProviderHandler {
             GoogleSignInAccount account = task.getResult(ApiException.class);
             String idToken = account.getIdToken();
             AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-            pluginImplementation.handleSuccessfulSignIn(call, credential, idToken);
+            String accessToken = getAccessTokenForAccount(account);
+            pluginImplementation.handleSuccessfulSignIn(call, credential, idToken, null, accessToken);
         } catch (ApiException exception) {
             pluginImplementation.handleFailedSignIn(call, null, exception);
+        }
+    }
+
+    private String getAccessTokenForAccount(GoogleSignInAccount account) {
+        String scope = "oauth2:" + Scopes.EMAIL + " " + Scopes.PROFILE;
+        try {
+            return GoogleAuthUtil.getToken(pluginImplementation.getPlugin().getContext(), account.getAccount(), scope, new Bundle());
+        } catch (GoogleAuthException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
