fc2d6c8c20ea5683655c2d60f144ceb845cf216a
Robin Genz <mail@robingenz.dev>
2022-11-05 23:36:43 +0100

    wip: ios

--- packages/app-check/ios/Plugin/FirebaseAppCheckPlugin.m
+++ packages/app-check/ios/Plugin/FirebaseAppCheckPlugin.m
@@ -4,5 +4,7 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(FirebaseAppCheckPlugin, "FirebaseAppCheck",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getToken, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(initialize, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setTokenAutoRefreshEnabled, CAPPluginReturnPromise);
 )
--- packages/app-check/ios/Plugin/FirebaseAppCheckPlugin.swift
+++ packages/app-check/ios/Plugin/FirebaseAppCheckPlugin.swift
@@ -7,14 +7,46 @@ import Capacitor
  */
 @objc(FirebaseAppCheckPlugin)
 public class FirebaseAppCheckPlugin: CAPPlugin {
+    public let errorEnabledMissing = "enabled must be provided."
+    public let errorGetTokenFailed = "Failed to get token."
     private var implementation: FirebaseAppCheck?
 
     override public func load() {
         implementation = FirebaseAppCheck()
     }
 
-    @objc func echo(_ call: CAPPluginCall) {
-        implementation?.startTrace(traceName)
+    @objc func getToken(_ call: CAPPluginCall) {
+        let forceRefresh = call.getBool("forceRefresh") ?? false
+        implementation?.getToken(forceRefresh: forceRefresh, completion: { token, expireTimeMillis, error in
+            if let error = error {
+                call.reject(error.localizedDescription)
+                return
+            }
+            guard let token = token else {
+                call.reject(self.errorGetTokenFailed)
+                return
+            }
+            var result = JSObject()
+            result["token"] = token
+            result["expireTimeMillis"] = expireTimeMillis
+            call.resolve(result)
+        })
+    }
+
+    @objc func initialize(_ call: CAPPluginCall) {
+        let debug = call.getBool("debug") ?? false
+        let isTokenAutoRefreshEnabled = call.getBool("isTokenAutoRefreshEnabled") ?? false
+        implementation?.initialize(debug: debug)
+        implementation?.setTokenAutoRefreshEnabled(isTokenAutoRefreshEnabled)
+        call.resolve()
+    }
+
+    @objc func setTokenAutoRefreshEnabled(_ call: CAPPluginCall) {
+        guard let enabled = call.getBool("enabled") else {
+            call.reject(errorEnabledMissing)
+            return
+        }
+        implementation?.setTokenAutoRefreshEnabled(enabled)
         call.resolve()
     }
 }
