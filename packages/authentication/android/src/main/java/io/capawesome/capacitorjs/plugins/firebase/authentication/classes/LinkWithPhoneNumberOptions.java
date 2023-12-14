07d038a12660637ca0884e5271fe434ecdd0971e
Robin Genz <mail@robingenz.dev>
2023-12-14 10:25:41 +0100

    feat(authentication): add `timeout` option to `signInWithPhoneNumber(…)` (#522)
    * feat(authentication): add `timeout` option to `signInWithPhoneNumber(...)`
    
    * docs [skip ci]
    
    * docs: add changeset [skip ci]

--- packages/authentication/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/authentication/classes/LinkWithPhoneNumberOptions.java
+++ packages/authentication/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/authentication/classes/LinkWithPhoneNumberOptions.java
@@ -1,8 +1,10 @@
 package io.capawesome.capacitorjs.plugins.firebase.authentication.classes;
 
+import androidx.annotation.NonNull;
+
 public class LinkWithPhoneNumberOptions extends SignInWithPhoneNumberOptions {
 
-    public LinkWithPhoneNumberOptions(String phoneNumber, boolean resendCode) {
-        super(false, phoneNumber, resendCode);
+    public LinkWithPhoneNumberOptions(String phoneNumber, boolean resendCode, @NonNull Long timeout) {
+        super(false, phoneNumber, resendCode, timeout);
     }
 }
