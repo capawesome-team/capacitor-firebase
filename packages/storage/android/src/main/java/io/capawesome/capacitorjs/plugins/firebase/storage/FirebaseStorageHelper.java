688792b364e19dabfa7a11590a0f606035cf00bb
Robin Genz <mail@robingenz.dev>
2024-06-05 09:49:02 +0200

    fix(storage): `JSONException` when `customMetadata` is not set

--- packages/storage/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/storage/FirebaseStorageHelper.java
+++ packages/storage/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/storage/FirebaseStorageHelper.java
@@ -19,8 +19,8 @@ public class FirebaseStorageHelper {
         String contentEncoding = metadata.getString("contentEncoding");
         String contentLanguage = metadata.getString("contentLanguage");
         String contentType = metadata.getString("contentType");
-        JSONObject customMetadata = metadata.getJSONObject("customMetadata");
-
+        JSONObject customMetadata = metadata.getJSObject("customMetadata");
+        
         StorageMetadata.Builder builder = new StorageMetadata.Builder();
         if (cacheControl != null) {
             builder = builder.setCacheControl(cacheControl);
