002758f3a8d49c28644fbccb89550a6f095bb64d
jgessinger@aeins.de <jgessinger@aeins.de>
2024-06-11 15:40:56 +0200

    fix(analytics): Fixed setConsent signature issues

---
+++ .changeset/chilly-coins-smash.md
@@ -0,0 +1,5 @@
+---
+'@capacitor-firebase/analytics': minor
+---
+
+fix(analytics): Fixed setConsent signature issues
--- packages/analytics/ios/Plugin/FirebaseAnalyticsPlugin.m
+++ packages/analytics/ios/Plugin/FirebaseAnalyticsPlugin.m
@@ -5,6 +5,7 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(FirebaseAnalyticsPlugin, "FirebaseAnalytics",
            CAP_PLUGIN_METHOD(getAppInstanceId, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setConsent, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(setUserId, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(setUserProperty, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(setCurrentScreen, CAPPluginReturnPromise);
