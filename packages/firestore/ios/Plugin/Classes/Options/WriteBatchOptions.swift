134ed3511d88c794e708de09e70447f08e861c20
Robin Genz <mail@robingenz.dev>
2024-06-17 17:07:39 +0200

    fix

--- packages/firestore/ios/Plugin/Classes/Options/WriteBatchOptions.swift
+++ packages/firestore/ios/Plugin/Classes/Options/WriteBatchOptions.swift
@@ -5,14 +5,14 @@ import Capacitor
     private var operations: [WriteBatchOperation]
 
     init(operations: [JSObject]) {
-        self.operations = createWriteBatchOperationArrayFromJSArray(operations)
+        self.operations = WriteBatchOptions.createWriteBatchOperationArrayFromJSArray(operations)
     }
 
     public func getOperations() -> [WriteBatchOperation] {
         return self.operations
     }
 
-    private func createWriteBatchOperationArrayFromJSArray(_ data: [JSObject]) -> [WriteBatchOperation] {
+    private static func createWriteBatchOperationArrayFromJSArray(_ data: [JSObject]) -> [WriteBatchOperation] {
         var operations: [WriteBatchOperation] = []
         for item in data {
             operations.append(WriteBatchOperation(item))
