f47df2f97add2dc9282b5f0df498f6e9b769959d
Robin Genz <mail@robingenz.dev>
2024-06-24 20:45:52 +0200

    fixes

--- packages/functions/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/functions/classes/results/CallResult.java
+++ packages/functions/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/functions/classes/results/CallResult.java
@@ -2,7 +2,13 @@ package io.capawesome.capacitorjs.plugins.firebase.functions.classes.results;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 import io.capawesome.capacitorjs.plugins.firebase.functions.interfaces.Result;
 
 public class CallResult implements Result {
@@ -16,10 +22,49 @@ public class CallResult implements Result {
 
     @NonNull
     public JSObject toJSObject() {
-        Object dataResult = this.data;
+        Object dataResult = CallResult.createJSValue(data);
 
         JSObject result = new JSObject();
         result.put("data", dataResult);
         return result;
+    }
+
+    @Nullable
+    private static Object createJSValue(@Nullable Object object) {
+        if (object instanceof Map) {
+            return createJSObjectFromMap((Map<String, Object>) object);
+        } else if (object instanceof ArrayList) {
+            return createJSArrayFromArrayList((ArrayList) object);
+        }
+        return object;
+    }
+
+    @Nullable
+    private static JSObject createJSObjectFromMap(@Nullable Map<String, Object> map) {
+      if (map == null) {
+        return null;
+      }
+      JSObject object = new JSObject();
+      for (String key : map.keySet()) {
+        Object value = map.get(key);
+        if (value instanceof ArrayList) {
+          value = createJSArrayFromArrayList((ArrayList) value);
+        } else if (value instanceof Map) {
+          value = createJSObjectFromMap((Map<String, Object>) value);
+        }
+        object.put(key, value);
+      }
+      return object;
+    }
+
+    private static JSArray createJSArrayFromArrayList(ArrayList arrayList) {
+      JSArray array = new JSArray();
+      for (Object value : arrayList) {
+        if (value instanceof Map) {
+          value = createJSObjectFromMap((Map<String, Object>) value);
+        }
+        array.put(value);
+      }
+      return array;
     }
 }
--- packages/functions/ios/Plugin/Classes/Results/CallResult.swift
+++ packages/functions/ios/Plugin/Classes/Results/CallResult.swift
@@ -10,7 +10,17 @@ import Capacitor
 
     public func toJSObject() -> AnyObject {
         var result = JSObject()
-        result["data"] = data as! any JSValue
+        result["data"] = CallResult.createJSValue(value: data)
         return result as AnyObject
+    }
+    
+    private static func createJSValue(value: Any?) -> JSValue? {
+        guard let value = value else {
+            return nil
+        }
+        guard let value = JSTypes.coerceDictionaryToJSObject(["key": value]) as JSObject? else {
+            return nil
+        }
+        return value["key"]
     }
 }
