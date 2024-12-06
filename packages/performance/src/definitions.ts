67c0f3a14188306631fd48ce14d9afbf80ba64a0
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-12-06 23:35:46 +0100

    feat(performance): add missing methods  (#505)
    Add method definitions.

--- packages/performance/src/definitions.ts
+++ packages/performance/src/definitions.ts
@@ -30,6 +30,49 @@ export interface FirebasePerformancePlugin {
    * @since 0.1.0
    */
   isEnabled(): Promise<IsEnabledResult>;
+  /**
+   * Sets a custom attribute of a trace to a given value.
+   *
+   * @since 6.3.0
+   */
+  putAttribute(options: PutAttributeOptions): Promise<void>;
+  /**
+   * Returns the value of a custom attribute of a trace.
+   *
+   * @since 6.3.0
+   */
+  getAttribute(options: GetAttributeOptions): Promise<GetAttributeResult>;
+  /**
+   * Gets the all the custom attributes of a trace with their values.
+   *
+   * @since 6.3.0
+   */
+  getAttributes(options: GetAttributesOptions): Promise<GetAttributesResult>;
+  /**
+   * Removes a custom attribute from a trace given its name.
+   *
+   * @since 6.3.0
+   */
+  removeAttribute(options: RemoveAttributeOptions): Promise<void>;
+  /**
+   * Sets the value of a custom metric.
+   *
+   * @since 6.3.0
+   */
+  putMetric(options: PutMetricOptions): Promise<void>;
+  /**
+   * Get the value of a custom metric by name.
+   *
+   * @since 6.3.0
+   */
+  getMetric(options: GetMetricOptions): Promise<GetMetricResult>;
+
+  /**
+   * Records a trace given its name and options.
+   *
+   * @since 6.3.0
+   */
+  record(options: RecordOptions): Promise<void>;
 }
 
 /**
@@ -104,4 +147,145 @@ export interface IsEnabledResult {
    * @since 0.1.0
    */
   enabled: boolean;
+}
+
+export interface PutAttributeOptions {
+  /**
+   * Name of the trace to set its attribute.
+   *
+   * @since 6.3.0
+   */
+  traceName: string;
+  /**
+   * Name of the attribute to set its value.
+   *
+   * @since 6.3.0
+   */
+  attribute: string;
+  /**
+   * The value to set to the attribute.
+   *
+   * @since 6.3.0
+   */
+  value: string;
+}
+
+export interface GetAttributeOptions {
+  /**
+   * Name of the trace to set its attribute.
+   *
+   * @since 6.3.0
+   */
+  traceName: string;
+  /**
+   * Name of the attribute to retrieve its value.
+   *
+   * @since 6.3.0
+   */
+  attribute: string;
+}
+
+export interface GetAttributeResult {
+  /**
+   * The value of the custom attribute.
+   *
+   * @since 6.3.0
+   */
+  value: string | null;
+}
+
+export interface GetAttributesOptions {
+  /**
+   * Name of the trace to get its attributes.
+   *
+   * @since 6.3.0
+   */
+  traceName: string;
+}
+
+export interface GetAttributesResult {
+  /**
+   * A map of all custom attributes of a trace with their values.
+   *
+   * @since 6.3.0
+   */
+  value: { [k: string]: string };
+}
+
+export type RemoveAttributeOptions = GetAttributeOptions;
+
+export interface PutMetricOptions {
+  /**
+   * Name of the trace to set its metric.
+   *
+   * @since 6.3.0
+   */
+  traceName: string;
+  /**
+   * The metric name.
+   *
+   * @since 6.3.0
+   */
+  metricName: string;
+  /**
+   * The value to set for the metric.
+   * The given value is floored down to the nearest integer.
+   *
+   * @since 6.3.0
+   */
+  num: number;
+}
+
+export interface GetMetricOptions {
+  /**
+   * Name of the trace to get its metric.
+   *
+   * @since 6.3.0
+   */
+  traceName: string;
+  /**
+   * The metric name.
+   *
+   * @since 6.3.0
+   */
+  metricName: string;
+}
+
+export interface GetMetricResult {
+  /**
+   * The value of the metric if exists. If not it will be null.
+   *
+   * @since 6.3.0
+   */
+  value: number | null;
+}
+
+export interface RecordOptions {
+  /**
+   * Name of the trace to record.
+   *
+   * @since 6.3.0
+   */
+  traceName: string;
+  /**
+   * Start time of the trace since epoch in milliseconds.
+   *
+   * @since 6.3.0
+   */
+  startTime: number;
+  /**
+   * The duration of the trace in milliseconds.
+   *
+   * @since 6.3.0
+   */
+  duration: number;
+  /**
+   * An optional object that hold optional maps of custom metrics and attributes.
+   *
+   * @since 6.3.0
+   */
+  options?: {
+    metrics?: { [key: string]: number };
+    attributes?: { [key: string]: string };
+  };
 }
