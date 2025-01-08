19db7e5e91cf24dc2f616955bacbadcce89d52f8
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2025-01-08 22:54:55 +0100

    feat(firestore): add getCountFromServer method
    Implement method on android.

---
+++ packages/firestore/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/firestore/classes/options/GetCountFromServerOptions.java
@@ -0,0 +1,17 @@
+package io.capawesome.capacitorjs.plugins.firebase.firestore.classes.options;
+
+import androidx.annotation.NonNull;
+
+public class GetCountFromServerOptions {
+  @NonNull
+  private final String reference;
+
+  public GetCountFromServerOptions(@NonNull String reference) {
+    this.reference = reference;
+  }
+
+  @NonNull
+  public String getReference() {
+    return reference;
+  }
+}
