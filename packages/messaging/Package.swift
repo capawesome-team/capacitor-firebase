c4d5feafc6a46302cdc7dcc7e793ae888c01624d
Robin Genz <mail@robingenz.dev>
2025-01-27 13:10:49 +0100

    chore: update deployment target

--- packages/messaging/Package.swift
+++ packages/messaging/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorFirebaseMessaging",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "CapacitorFirebaseMessaging",
