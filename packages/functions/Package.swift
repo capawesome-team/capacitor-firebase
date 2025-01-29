dfe00ab03189a61f39d820bcf8978fbdc1ef7fa8
Robin Genz <mail@robingenz.dev>
2025-01-29 10:15:52 +0100

    fix(spm): update `capacitor-swift-pm` to `7.0.0`

--- packages/functions/Package.swift
+++ packages/functions/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["FirebaseFunctionsPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "6.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "11.7.0"))
     ],
     targets: [
