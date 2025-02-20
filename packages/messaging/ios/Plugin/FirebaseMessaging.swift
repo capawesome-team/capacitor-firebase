f8fbd2fbc7d9b78a60f87d1c66b6f70e224e2a5e
Robin Genz <mail@robingenz.dev>
2025-02-20 09:28:24 +0100

    fix(messaging): request all permissions regardless of the configured presentation options

--- packages/messaging/ios/Plugin/FirebaseMessaging.swift
+++ packages/messaging/ios/Plugin/FirebaseMessaging.swift
@@ -22,22 +22,7 @@ import FirebaseCore
     }
 
     public func requestPermissions(completion: @escaping (_ granted: Bool, _ error: Error?) -> Void) {
-        var options = UNAuthorizationOptions()
-        self.config.presentationOptions.forEach { option in
-            switch option {
-            case "alert":
-                options.insert(.alert)
-            case "badge":
-                options.insert(.badge)
-            case "sound":
-                options.insert(.sound)
-            case "criticalAlert":
-                options.insert(.criticalAlert)
-            default:
-                print("Unrecogizned authorization option: \(option)")
-            }
-        }
-        UNUserNotificationCenter.current().requestAuthorization(options: options) { granted, error in
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
             completion(granted, error)
         }
     }
