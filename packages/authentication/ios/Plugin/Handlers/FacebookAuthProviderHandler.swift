f32121e2f05ace32127574a27b65025b10de8e03
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2025-03-12 13:59:45 +0100

    fix(authentication): Bad `accessToken` when signing in via Facebook under iOS
    Replace classic facebook login with limited login.

--- packages/authentication/ios/Plugin/Handlers/FacebookAuthProviderHandler.swift
+++ packages/authentication/ios/Plugin/Handlers/FacebookAuthProviderHandler.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CryptoKit
 import Capacitor
 import FirebaseAuth
 #if RGCFA_INCLUDE_FACEBOOK
@@ -39,36 +40,79 @@ class FacebookAuthProviderHandler: NSObject {
         #if RGCFA_INCLUDE_FACEBOOK
         let scopes = call.getArray("scopes", String.self) ?? []
         DispatchQueue.main.async {
-            self.loginManager.logIn(permissions: ["email", "public_profile"] + scopes, from: self.pluginImplementation.getPlugin().bridge?.viewController) { result, error in
-                if let error = error {
+            let viewController = self.pluginImplementation.getPlugin().bridge?.viewController
+            let nonce = self.randomNonceString()
+            let configuration = LoginConfiguration(
+                permissions: ["email", "public_profile"] + scopes,
+                tracking: .limited,
+                nonce: self.sha256(nonce)
+            )
+            self.loginManager.logIn(viewController: viewController, configuration: configuration) { result in
+                switch result {
+                case .cancelled:
+                    if isLink == true {
+                        self.pluginImplementation.handleFailedLink(message: nil, error: nil)
+                    } else {
+                        self.pluginImplementation.handleFailedSignIn(message: nil, error: nil)
+                    }
+                    return
+                case .failed(let error):
                     if isLink == true {
                         self.pluginImplementation.handleFailedLink(message: nil, error: error)
                     } else {
                         self.pluginImplementation.handleFailedSignIn(message: nil, error: error)
                     }
                     return
-                }
-
-                guard let accessToken = result?.token else {
-                    if isLink == true {
-                        self.pluginImplementation.handleFailedLink(message: self.errorLinkCanceled, error: nil)
-                    } else {
-                        self.pluginImplementation.handleFailedSignIn(message: self.errorSignInCanceled, error: nil)
+                case .success:
+                    guard let idTokenString = AuthenticationToken.current?.tokenString else {
+                        if isLink == true {
+                            self.pluginImplementation.handleFailedLink(message: self.errorLinkCanceled, error: nil)
+                        } else {
+                            self.pluginImplementation.handleFailedSignIn(message: self.errorSignInCanceled, error: nil)
+                        }
+                        return
                     }
-                    return
-                }
-
-                let accessTokenString = accessToken.tokenString
-                let credential = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
-                if isLink == true {
-                    self.pluginImplementation.handleSuccessfulLink(credential: credential, idToken: nil, nonce: nil,
-                                                                   accessToken: accessTokenString, serverAuthCode: nil, displayName: nil, authorizationCode: nil)
-                } else {
-                    self.pluginImplementation.handleSuccessfulSignIn(credential: credential, idToken: nil, nonce: nil,
-                                                                     accessToken: accessTokenString, displayName: nil, authorizationCode: nil, serverAuthCode: nil)
+                    
+                    let credential = OAuthProvider.credential(providerID: .facebook, idToken: idTokenString, rawNonce: nonce)
+                    if isLink == true {
+                        self.pluginImplementation.handleSuccessfulLink(credential: credential, idToken: idTokenString, nonce: nonce, accessToken: nil, serverAuthCode: nil, displayName: nil, authorizationCode: nil)
+                    } else {
+                        self.pluginImplementation.handleSuccessfulSignIn(credential: credential, idToken: idTokenString, nonce: nonce, accessToken: nil, displayName: nil, authorizationCode: nil, serverAuthCode: nil)
+                    }
                 }
             }
         }
         #endif
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      var randomBytes = [UInt8](repeating: 0, count: length)
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+      if errorCode != errSecSuccess {
+        fatalError(
+          "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+        )
+      }
+
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+
+      let nonce = randomBytes.map { byte in
+        charset[Int(byte) % charset.count]
+      }
+
+      return String(nonce)
+    }
+    
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
     }
 }
