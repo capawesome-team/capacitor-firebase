325442c77ca9e7b07ab0cb9abe1637a498043e0f
Martin Donadieu <martindonadieu@gmail.com>
2025-12-04 14:26:55 +0000

    fix: update package name and version for CapacitorFirebaseAppCheck

--- packages/app-check/Package.swift
+++ packages/app-check/Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
-    name: "CapgoCapacitorFirebaseAppCheck",
+    name: "CapacitorFirebaseAppCheck",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "CapgoCapacitorFirebaseAppCheck",
-            targets: ["CapgoFirebaseAppCheckPlugin"])
+            name: "CapacitorFirebaseAppCheck",
+            targets: ["FirebaseAppCheckPlugin"])
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "CapgoFirebaseAppCheckPlugin",
+            name: "FirebaseAppCheckPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
@@ -24,8 +24,8 @@ let package = Package(
             ],
             path: "ios/Plugin"),
         .testTarget(
-            name: "CapgoFirebaseAppCheckPluginTests",
-            dependencies: ["CapgoFirebaseAppCheckPlugin"],
+            name: "FirebaseAppCheckPluginTests",
+            dependencies: ["FirebaseAppCheckPlugin"],
             path: "ios/PluginTests")
     ]
 )
