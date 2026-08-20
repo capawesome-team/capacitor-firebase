4ca372e0925bcd37df8694310acce830822055fb
Robin Genz <mail@robingenz.dev>
2026-08-20 08:13:45 +0200

    feat(authentication): add SPM traits to make the provider SDKs optional

--- packages/authentication/Package.swift
+++ packages/authentication/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.1
 import PackageDescription
 
 let package = Package(
@@ -8,6 +8,21 @@ let package = Package(
         .library(
             name: "CapacitorFirebaseAuthentication",
             targets: ["FirebaseAuthenticationPlugin"])
+    ],
+    traits: [
+        .default(enabledTraits: ["Google", "Facebook"]),
+        .trait(
+            name: "Lite",
+            description: "Excludes all optional third party SDKs."
+        ),
+        .trait(
+            name: "Google",
+            description: "Includes the Google Sign-In SDK."
+        ),
+        .trait(
+            name: "Facebook",
+            description: "Includes the Facebook SDK."
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
@@ -23,18 +38,22 @@ let package = Package(
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
-                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
-                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
-                .product(name: "FacebookLogin", package: "facebook-ios-sdk")
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS",
+                         condition: .when(traits: ["Google"])),
+                .product(name: "FacebookCore", package: "facebook-ios-sdk",
+                         condition: .when(traits: ["Facebook"])),
+                .product(name: "FacebookLogin", package: "facebook-ios-sdk",
+                         condition: .when(traits: ["Facebook"]))
             ],
             path: "ios/Plugin",
             swiftSettings: [
-                .define("RGCFA_INCLUDE_GOOGLE"),
-                .define("RGCFA_INCLUDE_FACEBOOK")
+                .define("RGCFA_INCLUDE_GOOGLE", .when(traits: ["Google"])),
+                .define("RGCFA_INCLUDE_FACEBOOK", .when(traits: ["Facebook"]))
             ]),
         .testTarget(
             name: "FirebaseAuthenticationPluginTests",
             dependencies: ["FirebaseAuthenticationPlugin"],
             path: "ios/PluginTests")
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
