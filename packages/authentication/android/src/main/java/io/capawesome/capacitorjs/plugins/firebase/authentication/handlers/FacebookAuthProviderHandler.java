24e868970298dc5a3d4b4bbc60487292b906a978
Robin Genz <mail@robingenz.dev>
2026-09-04 09:59:25 +0200

    fix(authentication): start Facebook sign-in via `LoginManager` on Android

--- packages/authentication/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/authentication/handlers/FacebookAuthProviderHandler.java
+++ packages/authentication/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/authentication/handlers/FacebookAuthProviderHandler.java
@@ -1,52 +1,70 @@
 package io.capawesome.capacitorjs.plugins.firebase.authentication.handlers;
 
-import android.content.Intent;
 import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.getcapacitor.JSArray;
-import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FacebookAuthProvider;
 import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthentication;
 import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
+import java.util.ArrayList;
 import java.util.List;
 import org.json.JSONException;
 
 public class FacebookAuthProviderHandler {
 
-    public static final int RC_FACEBOOK_AUTH = 0xface;
     public static final String ERROR_SIGN_IN_CANCELED = "Sign in canceled.";
     public static final String ERROR_LINK_CANCELED = "Link canceled.";
-    private FirebaseAuthentication pluginImplementation;
-    private CallbackManager mCallbackManager;
-    private LoginButton loginButton;
+
+    private final FirebaseAuthentication pluginImplementation;
+    private final CallbackManager callbackManager = CallbackManager.Factory.create();
+
+    @Nullable
+    private LoginManager loginManager;
 
     @Nullable
     private PluginCall savedCall;
 
-    @Nullable
-    private Boolean isLink;
+    private boolean isLink;
 
     public FacebookAuthProviderHandler(FirebaseAuthentication pluginImplementation) {
         this.pluginImplementation = pluginImplementation;
-        try {
-            mCallbackManager = CallbackManager.Factory.create();
-            loginButton = new LoginButton(pluginImplementation.getPlugin().getContext());
+    }
 
-            loginButton.setPermissions("email", "public_profile");
-            loginButton.registerCallback(
-                mCallbackManager,
+    public void signIn(PluginCall call) {
+        logIn(call, false);
+    }
+
+    public void link(PluginCall call) {
+        logIn(call, true);
+    }
+
+    public void signOut() {
+        LoginManager.getInstance().logOut();
+    }
+
+    private void logIn(PluginCall call, boolean isLink) {
+        this.savedCall = call;
+        this.isLink = isLink;
+        getLoginManager().logIn(pluginImplementation.getPlugin().getActivity(), callbackManager, getPermissions(call), null);
+    }
+
+    @NonNull
+    private LoginManager getLoginManager() {
+        if (loginManager == null) {
+            loginManager = LoginManager.getInstance();
+            loginManager.registerCallback(
+                callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
-                    public void onSuccess(LoginResult loginResult) {
+                    public void onSuccess(@NonNull LoginResult loginResult) {
                         handleSuccessCallback(loginResult);
                     }
 
@@ -56,68 +74,46 @@ public class FacebookAuthProviderHandler {
                     }
 
                     @Override
-                    public void onError(FacebookException exception) {
+                    public void onError(@NonNull FacebookException exception) {
                         handleErrorCallback(exception);
                     }
                 }
             );
-        } catch (Exception exception) {
-            Log.e(FirebaseAuthenticationPlugin.TAG, "initialization failed.", exception);
         }
+        return loginManager;
     }
 
-    public void signIn(PluginCall call) {
-        this.savedCall = call;
-        this.isLink = false;
-        this.applySignInOptions(call, this.loginButton);
-        this.loginButton.performClick();
-    }
-
-    public void link(PluginCall call) {
-        this.savedCall = call;
-        this.isLink = true;
-        this.applySignInOptions(call, this.loginButton);
-        this.loginButton.performClick();
-    }
-
-    public void signOut() {
-        LoginManager.getInstance().logOut();
-    }
-
-    public void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void applySignInOptions(final PluginCall call, LoginButton button) {
+    @NonNull
+    private List<String> getPermissions(PluginCall call) {
+        List<String> permissions = new ArrayList<>();
+        permissions.add("email");
+        permissions.add("public_profile");
         JSArray scopes = call.getArray("scopes");
         if (scopes != null) {
             try {
-                List<String> scopeList = scopes.toList();
-                scopeList.add("email");
-                scopeList.add("public_profile");
-                button.setPermissions(scopeList);
+                permissions.addAll(scopes.toList());
             } catch (JSONException exception) {
-                Log.e(FirebaseAuthenticationPlugin.TAG, "applySignInOptions failed.", exception);
+                Log.e(FirebaseAuthenticationPlugin.TAG, "getPermissions failed.", exception);
             }
         }
+        return permissions;
     }
 
     private void handleSuccessCallback(LoginResult loginResult) {
-        AccessToken accessToken = loginResult.getAccessToken();
-        String accessTokenString = accessToken.getToken();
-        AuthCredential credential = FacebookAuthProvider.getCredential(accessTokenString);
-        if (savedCall == null || isLink == null) {
+        if (savedCall == null) {
             return;
         }
+        String accessToken = loginResult.getAccessToken().getToken();
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken);
         if (isLink) {
-            pluginImplementation.handleSuccessfulLink(savedCall, credential, null, null, accessTokenString, null);
+            pluginImplementation.handleSuccessfulLink(savedCall, credential, null, null, accessToken, null);
         } else {
-            pluginImplementation.handleSuccessfulSignIn(savedCall, credential, null, null, accessTokenString, null, null);
+            pluginImplementation.handleSuccessfulSignIn(savedCall, credential, null, null, accessToken, null, null);
         }
     }
 
     private void handleCancelCallback() {
-        if (savedCall == null || isLink == null) {
+        if (savedCall == null) {
             return;
         }
         if (isLink) {
@@ -128,7 +124,7 @@ public class FacebookAuthProviderHandler {
     }
 
     private void handleErrorCallback(FacebookException exception) {
-        if (savedCall == null || isLink == null) {
+        if (savedCall == null) {
             return;
         }
         if (isLink) {
